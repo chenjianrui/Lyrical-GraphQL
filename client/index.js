736f34d12d319f1e587e281f07e4e65500f87ad2
chenjianrui <sky75217@gmail.com>
2020-09-23 22:13:46 +0800

    initial apollo client and apollo provider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        Lyrical
+      </div>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
