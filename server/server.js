6e6e650ad9562475fb5bfb71150572f5b415985a
chenjianrui <sky75217@gmail.com>
2020-09-23 09:31:26 +0800

    新增 dotenv library 跟修正 GraphQL 與 mongoose 的語法

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const models = require('./models');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 
 const app = express();
-
+require('dotenv').config()
 // Replace with your mongoLab URI
-const MONGO_URI = '';
-if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+
+const connectDB = async () => {
+  const connection = await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  })
+
+  console.log(`MongoDB Connected: ${connection.connection.host}`)
 }
-
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
-
+connectDB()
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
